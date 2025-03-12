a389e3776b68e7de46e3dc6a74023d111c9e90e4
Mahdi Boulila <39028499+MahdiBoulila@users.noreply.github.com>
2025-03-12 15:39:48 -0400

    Update aboutme.ts

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -23,7 +23,7 @@ export const aboutMe: AboutMe = {
   institution: "Brown University",
   // Note that links work in the description
   description:
-    "I'm a second-year Computer Engineering <a href='https://www.brown.edu'>PhD candidate</a> working at the intersection of hardware acceleration and efficient machine learning. My research focuses on efficient deep learning, exploring neural network compression techniques such as quantization and sparsification to optimize models for deployment on edge devices. I have worked on multi-task learning, large language model optimization, and knowledge transfer. I aim to develop scalable, energy-efficient AI systems.",
+    "I'm a second-year Computer Engineering <a href='https://www.brown.edu'>PhD candidate</a> working at the intersection of hardware acceleration and machine learning. My research focuses on efficient deep learning, exploring neural network compression techniques such as quantization and sparsification to optimize models for deployment on edge devices. I have worked on multi-task learning, large language model optimization, and knowledge transfer. I aim to develop scalable, energy-efficient AI systems.",
   email: "mahdi_boulila[at]brown.edu",
   imageUrl:
     "https://i.ibb.co/pjPwTzg7/Portrait-Shot-2024.jpg",

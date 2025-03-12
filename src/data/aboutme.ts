592f3e93c715fd154ac08cdcbfc6c93b837147f9
Mahdi Boulila <39028499+MahdiBoulila@users.noreply.github.com>
2025-03-12 15:10:25 -0400

    Update aboutme.ts

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -26,8 +26,8 @@ export const aboutMe: AboutMe = {
     "I'm a second-year Computer Engineering <a href='https://www.brown.edu'>PhD candidate</a> working at the intersection of hardware acceleration and efficient machine learning. My research focuses on efficient deep learning, exploring neural network compression techniques such as quantization and sparsification to optimize models for deployment on edge devices. I have worked on multi-task learning, large language model optimization, and knowledge transfer. I aim to develop scalable, energy-efficient AI systems.",
   email: "mahdi_boulila[at]brown.edu",
   imageUrl:
-    "https://media.licdn.com/dms/image/v2/D4D03AQGtJj3AyT7jkg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1721669648722?e=1747267200&v=beta&t=PO121MbkEyxRfFmsLIfe12cjNt6Dvnq9R88Q59qR8ss",
-  googleScholarUrl: "https://scholar.google.com/citations?user=bWtMl_MAAAAJ",
+    "img.jpg",
+  googleScholarUrl: "https://scholar.google.com/citations?user=rSggvX0AAAAJ",
   githubUsername: "MahdiBoulila",
   linkedinUsername: "mahdiboulila",
   twitterUsername: "boulila_mahdi",

ec6a9455b60105e93d10fe2f7b7d4157e9c36554
Mahdi Boulila <39028499+MahdiBoulila@users.noreply.github.com>
2025-03-12 15:30:47 -0400

    Update news.ts

--- src/data/news.ts
+++ src/data/news.ts
@@ -7,9 +7,15 @@ export interface News {
 
 export const newsData: News[] = [
   {
-    date: "March 2024",
+    date: "March 2025",
     title: "Paper under-review at ICML 2025",
     description: "Our work on quantization and pruning is under-review at ICML.",
     link: "https://icml.cc/",
+  },
+  {
+    data: "September 2024",
+    title: "How to get a Ph.D. in the US for free!",
+    description: "Presented a virtual talk on navigating US PhD admissions covering application fee waivers, funding opportunities, and strategic planning.",
+    link: "https://www.linkedin.com/company/openmindsproject-org/posts/",
   }
 ];
